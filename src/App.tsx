b033c1dbb652267e866aa10c87bdc8a6b1627873
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react.svg";
+import { Text } from "@mantine/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Text>Welcome to Mantine!</Text>
+    </div>
+  );
+}
+
+export default App;
